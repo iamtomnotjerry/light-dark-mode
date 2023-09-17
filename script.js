e50fd34759853e5cb01c7b646fc7d6177feb8968
Tom <talama112335@gmail.com>
2023-09-17 16:06:58 +0700

    changing

--- script.js
+++ script.js
@@ -6,38 +6,40 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
-// Dark Mode Styles
-function darkMode() {
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark mode';
-  toggleIcon.children[1].classList.remove('fa-sun');
-  toggleIcon.children[1].classList.add('fa-moon')
-  image1.src = "img/undraw_proud_coder_dark.svg";
-  image2.src = "img/undraw_feeling_proud_dark.svg";
-  image3.src = "img/undraw_conceptual_idea_dark.svg";
+// Dark or light Images
+function imageMode(color) {
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Light mode styles
-function lightMode() {
-  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  toggleIcon.children[0].textContent = "Light mode";
-  toggleIcon.children[1].classList.remove("fa-moon");
-  toggleIcon.children[1].classList.add("fa-sun");
-  image1.src = "img/undraw_proud_coder_light.svg";
-  image2.src = "img/undraw_feeling_proud_light.svg";
-  image3.src = "img/undraw_conceptual_idea_light.svg";
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark
+    ? "rgb(0 0 0 / 50%)"
+    : "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = isDark
+    ? "rgb(255 255 255 / 50%"
+    : "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = isDark 
+    ? "Dark mode" 
+    : "light mode";
+  isDark
+    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[1].classList.replace("fa-moon","fa-sun");
+  isDark ? imageMode("dark") : imageMode('light');
 }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    darkMode();
+    localStorage.setItem('theme', 'dark');
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    lightMode();
+        localStorage.setItem("theme", "light");
+
+    toggleDarkLightMode(false);
 }
 }
 
@@ -45,3 +47,13 @@ function switchTheme(event) {
 
 // Event Listener 
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
